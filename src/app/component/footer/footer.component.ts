e11f0143ca37b3ed24fe7f29e04872d7bb7aaae9
Abhilash A S <abhilashatl57@gmail.com>
2024-08-11 19:55:02 +0530

    Footer changes

--- src/app/component/footer/footer.component.ts
+++ src/app/component/footer/footer.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
+  private DEFAULT_TOOLTIP_NAME = 'Powered by Abhilash Swathy';
+  public tooltipText: string = this.DEFAULT_TOOLTIP_NAME;
 
+  onMouseEnter() {
+    this.tooltipText = this.DEFAULT_TOOLTIP_NAME;
+  }
+  onDoubleClick(){
+    console.log("oooooooooo")
+    this.tooltipText = "Powered by Abhilash Swathy";
+  }
 }
